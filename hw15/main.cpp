b66eafce0b35aaf189748b4062c039f3d0f47694
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 03:14:38 +0100

    task 2 done and working

--- hw15/main.cpp
+++ hw15/main.cpp
@@ -5,40 +5,61 @@ using namespace std;
 int main() {
 
     // task 1
-    int integer;
-    float realNumber;
-    string word;
+    //int integer;
+    //float realNumber;
+    //string word;
 
-    cout << "Enter integer number: ";
-    cin >> integer;
+    //cout << "Enter integer number: ";
+    //cin >> integer;
 
-    cout << "Enter natural number: ";
-    cin >> realNumber;
+    //cout << "Enter natural number: ";
+    //cin >> realNumber;
 
-    cout << "Enter a word: ";
-    cin >> word;
+    //cout << "Enter a word: ";
+    //cin >> word;
 
-    ofstream file("1.txt");
+    //ofstream file("1.txt");
+
+    //if (file.is_open()) {
+    //    for (int i = 0; i < 5; ++i) {
+    //        file << integer << " ";
+    //    }
+    //    for (int i = 0; i < 5; ++i) {
+    //        file << realNumber << " ";
+    //    }
+    //    for (int i = 0; i < 5; ++i) {
+    //        file << word << " ";
+    //    }
+
+    //    file.close();
+    //    cout << "Data written to file 1.txt" << endl;
+    //}
+    //else {
+    //    cout << "Error opening file" << endl;
+    //}
+
+
+    // task 2
+    ofstream file("2.txt");
 
     if (file.is_open()) {
-        for (int i = 0; i < 5; ++i) {
-            file << integer << " ";
-        }
-        for (int i = 0; i < 5; ++i) {
-            file << realNumber << " ";
-        }
-        for (int i = 0; i < 5; ++i) {
-            file << word << " ";
+        int a = 1, b = 1;
+        for (int i = 0; i < 12; ++i) {
+            file << a << " ";
+            int c = a + b;
+            a = b;
+            b = c;
         }
 
         file.close();
-        cout << "Data written to file 1.txt" << endl;
+        cout << "Fibonacci numbers written to file 2.txt" << endl;
     }
     else {
         cout << "Error opening file" << endl;
     }
 
-
+    
+    
 
 
 
