a54070080f55bff948a1c610c05c9719fc12e855
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 03:12:10 +0100

    task 1 done and working

--- hw15/main.cpp
+++ hw15/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
 
+    // task 1
+    int integer;
+    float realNumber;
+    string word;
+
+    cout << "Enter integer number: ";
+    cin >> integer;
+
+    cout << "Enter natural number: ";
+    cin >> realNumber;
+
+    cout << "Enter a word: ";
+    cin >> word;
+
+    ofstream file("1.txt");
+
+    if (file.is_open()) {
+        for (int i = 0; i < 5; ++i) {
+            file << integer << " ";
+        }
+        for (int i = 0; i < 5; ++i) {
+            file << realNumber << " ";
+        }
+        for (int i = 0; i < 5; ++i) {
+            file << word << " ";
+        }
+
+        file.close();
+        cout << "Data written to file 1.txt" << endl;
+    }
+    else {
+        cout << "Error opening file" << endl;
+    }
+
+
+
+
+
+
+    return 0;
 
 }
